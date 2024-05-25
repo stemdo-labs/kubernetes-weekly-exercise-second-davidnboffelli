2ccc6322da63ad793fd23015f60104501e90afbf
David Boffelli <dnazareno@stemdo.io>
2024-05-25 16:59:52 +0200

    Subo Ingress, README y 2 values de ejemplo.

---
+++ soluciones/comentarios.sh
@@ -0,0 +1,3 @@
+# Subo imagenes donde se puede ver el funcionamiento del ingress en el navegador. "ingressLaravel.png" e "ingressPhpmyadmin.png".
+# Como se puede ver en la imagen, phpmyadmin me devuelve una pantalla en blanco. No supe resolverlo.
+# Si accedo directo al servicio de phpmyadmin, funciona correctamente. Adjunto imagen "phpmyadminService.png"
